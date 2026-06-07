7b88e8a97d3e3b3dfb35dd7aee7b4622e856c248
yysam123456 <yysam123456@users.noreply.github.com>
2026-06-07 16:34:30 +0800

    fix: add Resources to navbar navigation

--- config/marketing.ts
+++ config/marketing.ts
@@ -11,6 +11,9 @@ export const marketingConfig: MarketingConfig = {
       href: "/tools",
     },
     {
+      title: "Resources",
+      href: "/resources",
+    },
       title: "Directory",
       href: "/directory",
     },
